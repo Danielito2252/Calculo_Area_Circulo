1477a546d45a4f15b28887f7c18aeb00cc2fd1d2
Danielito2252 <hbarriosb4@miumg.edu.gt>
2023-05-05 23:05:22 -0600

    No se ha modificado nada

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//PROGRAMA QUE CALCULA  AREA DE UN CIRCULO
+Console.WriteLine("¡vamos a calcular el area de un circulo!");
+
+//DEFINIMOS LAS VARIABLES
+double radio;
+double area;
+string mensaje = "";
+double PI = 3.1416;
+
+    Console.WriteLine("Escriba o ingrese el valor del radio");
+radio = Convert.ToDouble(Console.ReadLine());
+
+ area = Math.PI * Math.Pow(radio, 2);
+
+Console.WriteLine($"El radio del circulo es: {radio} y el area es: {area}");
